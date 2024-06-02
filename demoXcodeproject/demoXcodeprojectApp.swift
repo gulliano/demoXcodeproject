23515b6902b3b31d90306435d33b2377a6718b55
gulliano <ggulliano@msn.com>
2024-06-02 19:24:10 -0400

    Initial Commit

---
+++ demoXcodeproject/demoXcodeprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoXcodeprojectApp.swift
+//  demoXcodeproject
+//
+//  Created by gulliano on 02/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct demoXcodeprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
